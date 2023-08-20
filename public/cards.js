61d2e7369861ec04d2a182802e33a4c7df8e28c7
jamesmedina7787 <jameswilfredmedina@gmail.com>
2023-08-20 13:48:58 -0400

    progress on the loading images in all galleries

--- public/cards.js
+++ public/cards.js
@@ -1,40 +1,17 @@
-const cardRow = document.querySelector('.card-row');
-
-// Sample card data
-const cards = [
-    { id: 1, imageUrl: '/imgs/drums.gif' },
-    { id: 2, imageUrl: '/imgs/005.gif' },
-    { id: 3, imageUrl: '/imgs/drums.gif' },
-];
-
-// Function to create card elements
-function createCard(cardData) {
-    const card = document.createElement('div');
-    card.classList.add('card');
-
-    const cardInner = document.createElement('div');
-    cardInner.classList.add('card-inner');
-
-    const cardFront = document.createElement('div');
-    cardFront.classList.add('card-front');
-
-    const cardBack = document.createElement('div');
-    cardBack.classList.add('card-back');
-
-    const img = document.createElement('img');
-    img.src = cardData.imageUrl;
-
-    cardFront.appendChild(img);
-
-    cardInner.appendChild(cardFront);
-    cardInner.appendChild(cardBack);
-
-    card.appendChild(cardInner);
-
-    cardRow.appendChild(card); // Append the card to the card row
+const blurDivs = document.querySelectorAll("#small")
+console.log("helo?")
+blurDivs.forEach(div =>
+div.querySelectorAll("img")
+  function loaded() {
+    div.classList.add("loaded")
+    console.log("well")
+  }
+if(img.complete){
+loaded()
+console.log("hello")
 }
-
+ else {
+img.addEventListener("load", loaded)
+}
+)
 // Load cards on page load
-window.onload = () => {
-    cards.forEach(cardData => createCard(cardData));
-};
